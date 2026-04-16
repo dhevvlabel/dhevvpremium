d4e1cb54fd0e5b9dd31d1e1c73cd5bd2c34afa06
dhevvlabel <dffaseptember@gmail.com>
2026-04-17 05:30:35 +0700

    Add files via upload

---
+++ vite.config (1).ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    // Load env variables from .env files, but ALSO include process.env for system/Vercel variables
+    const env = {
+      ...process.env,
+      ...loadEnv(mode, process.cwd(), '')
+    };
+
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || env.VITE_GEMINI_API_KEY),
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
